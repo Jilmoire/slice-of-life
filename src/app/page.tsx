b4de7ef30785787c1e14f143ded35424f53c7e6e
Jill <guilmarlopez2007@gmail.com>
2026-07-14 22:19:45 +0800

    Create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
-import Button from '@mui/material/Button';
+//mui modules
+import ImageList from '@mui/material/ImageList';
+import Container from '@mui/material/Container';
+import ImageListItem from '@mui/material/ImageListItem';
+import Typography from '@mui/material/Typography';
+
+//custom modules
+import { NavBar } from '@/modules/assets/AppBar';
 
 export default function ButtonUsage() {
-  return <Button variant="contained">Hello world</Button>;
+  return (
+    <html>
+      <head/>
+        <body>
+          <NavBar></NavBar>
+
+          <Container>
+            <ImageList>
+              <ImageListItem>
+                <img></img>
+              </ImageListItem>
+            </ImageList>
+
+            <Typography>
+              IMAGE DESCRIPTION OR SOMTHIN
+            </Typography>
+
+          </Container>
+
+
+        </body>
+    </html>
+  );
 }
