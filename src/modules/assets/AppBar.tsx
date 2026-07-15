ff03164e248610a086b3f68595e3248add7b26f2
Jill <guilmarlopez2007@gmail.com>
2026-07-15 19:39:04 +0800

    update appbar

--- src/modules/assets/AppBar.tsx
+++ src/modules/assets/AppBar.tsx
@@ -7,7 +7,7 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Container from '@mui/material/Container';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
-
+import Box from '@mui/material/Box';
 
 //mui icons
 import CollectionsIcon from '@mui/icons-material/Collections';
@@ -16,26 +16,30 @@ import CollectionsIcon from '@mui/icons-material/Collections';
 export function NavBar() {
   return (
     <AppBar position="static">
-      <Container maxWidth="false">
-      <Toolbar>
-        <IconButton>
-          <CollectionsIcon/> 
-        </IconButton>
-        <Typography>
-          <Link href='#' color='textPrimary' underline='none'>Slice of Life</Link>
-        </Typography>
+      <Container maxWidth={false}>
+      <Toolbar sx={{justifyContent: "space-between"}}>
+        <Box sx={{ display: "flex", alignItems: 'center', gap: 1 }}>
+          <IconButton>
+            <CollectionsIcon/> 
+          </IconButton>
+          <Typography>
+            <Link href="#" color="textPrimary" underline="none">Slice of Life</Link>
+          </Typography>
+        </Box>
 
-        <Breadcrumbs>
-          <Link underline="hover" color="inherit" href="#">
-            Sign In
-          </Link>
-          <Link underline="hover" color="inherit" href="#">
-            About
-          </Link>
-          <Link underline="hover" color="inherit" href="#">
-            Create an Account
-          </Link>
-        </Breadcrumbs>
+        <Box sx={{ display: "flex", alignItems: 'center', gap: 1 }}>
+          <Breadcrumbs>
+            <Link underline="hover" color="inherit" href="#">
+              Sign In
+            </Link>
+            <Link underline="hover" color="inherit" href="#">
+              About
+            </Link>
+            <Link underline="hover" color="inherit" href="#">
+              Create an Account
+            </Link>
+          </Breadcrumbs>
+        </Box>
 
       </Toolbar>
       </Container>
