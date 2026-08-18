c449b1bbff2dbb8dc49e647607faf91ad437e755
Jill <gmlopez2007@gmail.com>
2026-08-18 20:45:26 +0800

    Finish Foundational Component for Navbar

--- src/modules/navigation/NavBars.tsx
+++ src/modules/navigation/NavBars.tsx
@@ -1,12 +1,27 @@
+"use client";
+
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box'
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import MenuIcon from '@mui/icons-material/Menu';
 import Typography from '@mui/material/Typography';
 
 export function WelcomeBar() {
     return(
-        <AppBar sx={{padding:'1.5rem'}}>
-            <Box>icon - - - label</Box>
-            <Box> option   option   option  </Box>
+        <AppBar>
+            <Toolbar sx={{padding:'1.3rem'}}>
+                <IconButton edge="start" color="inherit" aria-label="menu" sx={{mr: 2, ml: 3}}>
+                    <MenuIcon />
+                </IconButton>
+                <Typography sx={{ flexGrow: '1'}}>Slice of Life</Typography>
+                
+                <Box sx={{ display: 'flex', gap: 3, mr: 4 }}>
+                    <Typography>Plans</Typography>
+                    <Typography>Services</Typography>
+                    <Typography>About</Typography>
+                </Box>
+            </Toolbar>
         </AppBar>
     )
 }
